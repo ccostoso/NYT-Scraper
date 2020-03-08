bda7cd05279449bd8c8377be05ebe91a1c000e6c
ccostoso <carlos.costoso@outlook.com>
2020-03-08 18:42:36 -0400

    Beginning of routes and Handlebars

---
+++ routes/html-routes.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+// Requiring our Todo model
+const db = require("../models");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+    db.User
+    .find({})
+    .populate("articles")
+    .then(dbUser => {
+        const hbsObject = {
+            user: dbUser
+        }
+
+        res.render("index", hbsObject);
+
+        console.log(hbsUser);
+    })
+
+    db.Burger.findAll()
+        .then(function(data) {
+            var hbsObject = {
+                burger: data
+            }
+            
+            res.render("index", hbsObject);
+
+            console.log(data);
+    });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
