c597fc4426a5e59081a3d9330f24ebe27cba9371
ccostoso <carlos.costoso@outlook.com>
2020-03-10 21:07:45 -0400

    Prevented database from adding more entires on each GET request if database already has entries. Added 'refresh database' functionality.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,5 +3,33 @@ $( document ).ready(function() {
         e.preventDefault();
         const id = $(this).data("id");
         console.log("_id:", id);
+
+        $.ajax({
+            url: '/:id',
+            method: 'PUT',
+            data: id
+        })
+        .then(function() {
+            location.reload();
+        })
     });
+
+    $("#reset").on("click", function(e) {
+        e.preventDefault();
+
+        $("main").empty();
+        
+        $.ajax({
+            url: '/reset',
+            method: 'DELETE',
+            data: id
+        }).then(function() {
+            $.ajax({
+                url: '/',
+                method: 'GET'
+            }).then(function(res) {
+                console.log(res);
+            })
+        })
+    })
 });
--- routes/routes.js
+++ routes/routes.js
@@ -6,95 +6,147 @@ const cheerio = require('cheerio');
 // Requiring our Todo model
 const db = require("../models");
 
+// Create renderHbs function so as not to repeat code on Handlebar rendering on GET requests
+const renderHbs = (articleRes, page, res) => {
+    const hbsObject = {
+        article: articleRes
+    }
+
+    res.render(page, hbsObject);
+    console.log("hbsObject:", hbsObject);
+}
+
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function (req, res) {
-    // First, we grab the body of the html with axios
-    // axios.get("http://www.nytimes.com/").then(function (response) {
-    //     // Then, we load that into cheerio and save it to $ for a shorthand selector
-    //     const $ = cheerio.load(response.data);
+router.get("/", (req, res) => {
 
-    //     // Now, we grab every h2 within a div (within an a) tag, and do the following:
-    //     $("a div h2").each(function (i, element) {
-    //         // Save an empty result object
-    //         let result = {};
+    // Query database to see if there are already documents in the Article collection
+    db.Article.find({})
+        .then(dbArticles => {
 
-    //         // Add the text and href of every link, and save them as properties of the result object
-    //         result.title = $(this)
-    //             .text();
-    //         result.subheading = $(this)
-    //             .parent()
-    //             .parent()
-    //             .find("p")
-    //             .text();
-    //         result.link = $(this)
-    //             .parent()
-    //             .parent()
-    //             .attr("href");
-    //         result.saved = false;
+            // If there are documents in the Article collection... 
+            if (dbArticles.length > 0) {
+                // ...render the results in the Handlebars.
+                renderHbs(dbArticles, "index", res);
 
-            // // Create a new Article using the `result` object built from scraping
-            // db.Article.create(result)
-            //     .then(function (dbArticle) {
-            //         // View the added result in the console
-            //         console.log(dbArticle);
-            //     })
-            //     .catch(function (err) {
-            //         // If an error occurred, log it
-            //         console.log(err);
-            //     });
+                // Otherwise, run Axios to provide entries for the Article collection.
+            } else {
+                // We grab the body of the html with axios
+                axios.get("http://www.nytimes.com/").then(response => {
 
-            db.Article.find({})
-                .then(function(dbArticles) {
-                    // Render the results to Handlebars
-                    const hbsObject = {
-                        article: dbArticles
-                    }
+                    // Then, we load that into cheerio and save it to $ for a shorthand selector
+                    const $ = cheerio.load(response.data);
 
-                    res.render("index", hbsObject);
-                    console.log("hbsObject:", hbsObject)
-                })
-                .catch(function(err) {
-                    console.log(err)
-                })
-        // });
-    // });
-});
+                    // Now, we grab every h2 within a div (within an a) tag, and do the following:
+                    $("a div h2").each((i, element) => {
+                        // Save an empty result object
+                        let result = {};
 
-router.get("/saved", function (req, res) {
-    db.Article.find({saved: true})
-        .then(function(dbArticles) {
-            // Render the results to Handlebars
-            const hbsObject = {
-                article: dbArticles
+                        // Add the text and href of every link, and save them as properties of the result object
+                        result.title = $(this)
+                            .text();
+                        result.subheading = $(this)
+                            .parent()
+                            .parent()
+                            .find("p")
+                            .text();
+                        result.link = "https://nytimes.com/" + $(this)
+                            .parent()
+                            .parent()
+                            .attr("href");
+                        result.saved = false;
+
+                        // Create a new Article using the `result` object built from scraping
+                        db.Article
+                            .create(result)
+                            .then(dbArticle => {
+                                // View the added result in the console
+                                console.log(dbArticle);
+                            })
+                            .catch(err => {
+                                // If an error occurred, log it
+                                console.log(err);
+                            });
+                            
+                        // Finally, retrieve all new entries...
+                        db.Article
+                            .find({})
+                            .then(dbArticles => {
+                                // ...and render the results in the Handlebars.
+                                renderHbs(dbArticles, "index", res);
+                            })
+                    });
+                });
             }
-
-            res.render("saved", hbsObject);
-            console.log("hbsObject:", hbsObject)
         })
-        .catch(function(err) {
-            console.log(err)
-        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
-router.put("/:id", function(req, res) {
-    const id = req.params.id;
-    db.Article
-        .updateOne({ _id: id }, { $set: { saved: true } })
-        .then(response => {
-            res.json(response);
+router.get("/saved", (req, res) => {
+    db.Article.find({ saved: true })
+        .then(dbArticles => {
+            // Render the results to Handlebars
+            renderHbs(dbArticles, "saved", res);
         })
         .catch(err => {
             console.log(err)
         })
+});
+
+router.put("/:id", (req, res) => {
+    const id = req.params.id;
+    db.Article
+    .findOne({
+        _id: id
+    })
+    .then(response => {
+        if (response.saved) {
+            db.Article
+            .updateOne({ _id: id }, { $set: { saved: false } })
+            .then(response => {
+                res.json(response);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        } else {
+            db.Article
+            .updateOne({ _id: id }, { $set: { saved: true } })
+            .then(response => {
+                res.json(response);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+
 })
 
-router.delete("/:id", function(req, res) {
+router.delete("/:id", (req, res) => {
     const id = req.params.id;
     db.Article
         .updateOne({ _id: id })
         .then(response => {
             res.json(response);
-        }).catch(err => {
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
+
+router.delete("/reset", (req, res) => {
+    db.Article
+        .remove({})
+        .then(response => {
+            res.json(response)
+        })
+        .catch(err => {
             console.log(err);
         })
 })
